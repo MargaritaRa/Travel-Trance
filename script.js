b265e4d2495732919a4bfd5d61c4b6fa4ebb8a6b
Margarita Ramirez <margarita26.mr@gmail.com>
2024-02-27 17:55:10 -0500

    skeleton plan

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "The story of why I changed my career";
 
 document.querySelector("body").appendChild(h2);
