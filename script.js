5ec1b2e073b18c704a2998ff74b85e41b97c256e
Margarita Ramirez <margarita26.mr@gmail.com>
2024-03-09 16:26:44 -0500

    updated slide show

--- script.js
+++ script.js
@@ -3,29 +3,31 @@ h2.textContent = "The story of why I changed my career";
 
 document.querySelector("body").appendChild(h2);
 
-let slider = tns({
-    container : ".my-slider",
-    "slideBy" : "1",
-    "speed" :400,
-    "nav" : false,
-    autoplay : true,
-    controls: false,
-    autoplayButtonOutput : false,
-    response: {
-        1600: {
-            items : 7,
-            gutter : 20
-        },
-        1024: {
-            items: 3,
-            gutter : 20
-        },
-        768: {
-            items : 2,
-            gutter 20
-        },
-        480: {
-            items: 1,
-        }
-    }
-})
+let slideIndex = 7;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
